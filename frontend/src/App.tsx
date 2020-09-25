1abf3cbc21c49058bcddbea4dce44e98c78a80a1
Yasser <yassergeo@gmail.com>
2020-09-25 18:31:11 -0500

    fixed form button

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,38 @@
 import React, { Fragment, useState } from "react";
+import { createNoSubstitutionTemplateLiteral } from "typescript";
+
 type FormElement = React.FormEvent<HTMLFormElement>;
 interface Itask {
   name: string;
   done: boolean;
 }
+
 function App(): JSX.Element {
   const [newTask, setNewTask] = useState<string>("");
   const [tasks, setTasks] = useState<Itask[]>([]);
+
   const handleSubmit = (e: FormElement) => {
     e.preventDefault();
+    addTask(newTask);
+    setNewTask("");
   };
+
   const addTask = (name: string) => {
     const newTasks: Itask[] = [...tasks, { name, done: false }];
     setTasks(newTasks);
   };
+
   return (
     <Fragment>
       <form onSubmit={handleSubmit}>
-        <input type="text" onChange={(e) => setNewTask(e.target.value)} />
+        <input
+          type="text"
+          onChange={(e) => setNewTask(e.target.value)}
+          value={newTask}
+        />
+
+        <button>Enviar</button>
       </form>
-      <button>Enviar</button>
     </Fragment>
   );
 }
